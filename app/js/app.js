1c058662e04f018ca106f28b62a71c9f251089bb
gh-aakimkin <aakimkin@mail.ru>
2020-03-05 17:14:59 +0300

    Начало верстки второй секции

--- app/js/app.js
+++ app/js/app.js
@@ -3,8 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
 	$('.menu-button').click(function() {
 		$('.menu').stop().slideToggle();
 	});
-
-	$('.menu').click(function(e){
+	$(document).keyup(function(e){
+		if (e.keyCode == 27){
+		$('.menu').slideUp();
+		}
+	}).click(function(){
+		$('.menu').slideUp();
+	});
+	$('.navbar').click(function(e){
 		e.stopPropagation();
 	});
 	
